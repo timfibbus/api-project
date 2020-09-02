9a9efcf00151c5f0f69e1d865134ac9689d776e6
maisiegallagher <maisiejgallagher@gmail.com>
2020-09-02 16:45:04 -0400

    second commit to try merging to master.

--- api-project/src/main/java/co/timfibbus/apiproject/FavoritesApiController.java
+++ api-project/src/main/java/co/timfibbus/apiproject/FavoritesApiController.java
@@ -1,7 +1,21 @@
 package co.timfibbus.apiproject;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FavoritesApiController {
 
-	
-	
-}
+	@Autowired
+	private FavoritesDao dao;
+
+	@GetMapping("/favorites")
+	public List<Recipe> listFavorites(@RequestParam(required=false) String label) {
+			return dao.findAll();
+		}
+	}
+
